d77a5b47692ab14d8f4b35d5a284a539229eef20
Mr.WhiteDevil <40131496+WhiteDevil-rss@users.noreply.github.com>
2026-03-23 03:09:33 +0530

    chore: untrack and ignore test/debug/check files

--- apps/api/check.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
-
-async function check() {
-    const faculty = await prisma.faculty.findMany({ select: { name: true } });
-    console.table(faculty);
-}
-check().then(() => prisma.$disconnect()).catch(console.error);
